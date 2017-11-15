327880ad046a82d33da7444abab8a2fa838bbe7a
om65praj <om65praj@gmail.com>
2017-11-15 19:21:52 +0530

    commit

--- src/main/webapp/Controller/loginController.js
+++ src/main/webapp/Controller/loginController.js
@@ -6,9 +6,12 @@ toDoApp.controller('loginController',
 			$scope.user = {};
 
 			$scope.loginUser = function() {
-
+				
+				console.log("User email is : "+$scope.user.email)
+				console.log("User password is : "+$scope.user.password)
+				
 				var loginVariable = loginService.loginuser($scope.user);
-
+				
 				loginVariable.then(function(response) {
 					console.log(response.data);
 					$location.path('/home')
