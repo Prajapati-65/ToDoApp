8b006edd2ead14faf77c0040fecc6ecc0aa2d0e1
om65praj <om65praj@gmail.com>
2017-11-22 06:27:42 +0530

    22/11

--- src/main/webapp/Service/homeService.js
+++ src/main/webapp/Service/homeService.js
@@ -14,5 +14,62 @@ todoApp.factory('homeService', function($http, $location){
 			data : notes
 		})
 	}
+
+	
+	cards.changeColor=function(note){
+		return $http({
+			method : "POST",
+			url : 'user/changeColor',
+			headers : {
+				'token' : localStorage.getItem('token')
+			},
+			data : note
+		})
+	}
+	
+	cards.deleteNoteForever = function(id){
+		return $http({
+			method : "DELETE",
+			url : 'user/delete/'+id,
+			headers: {
+				'token':localStorage.getItem('token')
+			}
+		})
+	}
+	
+	cards.logout = function(){
+		return $http({
+			method : "POST",
+			url : 'logout',
+			headers: {
+				'token':localStorage.getItem('token')
+			}
+		})
+	}
+	
+	cards.updateNote=function(notes){
+		console.log("inside the update service...");
+		return $http({
+			method : "POST",
+			url : 'user/update',
+			headers: {
+				'token':localStorage.getItem('token')
+			},
+			data: notes
+		})
+	}
+	
+	cards.allNotes = function() {
+		console.log("home page service2 all notes");
+		return $http({
+			method : "GET",
+			url : 'user/getallnotes',
+			headers: {
+				'token':localStorage.getItem('token')
+			}
+		})
+	}
+	
 	return cards;
+
 });
