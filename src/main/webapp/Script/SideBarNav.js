46e1ab38cd6f59cd1e697d9c58e6518b037ac769
om65praj <om65praj@gmail.com>
2017-11-27 15:04:18 +0530

    commit

---
+++ src/main/webapp/Script/SideBarNav.js
@@ -0,0 +1,23 @@
+/*
+function toggleSideBar(){
+	var sideNav=document.getElementById("sideToggle").style.width;
+	if(sideNav=="0px"){
+		openNav();
+	}
+	else{
+		closeNav();
+	}
+}
+
+
+function openNav() {
+    document.getElementById("sideToggle").style.width = "250px";
+    document.getElementById("noteWrapper").style.marginLeft = "250px";
+}
+
+
+function closeNav() {
+    document.getElementById("sideToggle").style.width = "0px";
+    document.getElementById("noteWrapper").style.marginLeft = "0px";
+}
+*/
