779ebf63fbdfbcd018d84cf74376a06573fe77a7
om65praj <om65praj@gmail.com>
2017-12-13 17:44:34 +0530

    new commit

--- src/main/webapp/Controller/homeController.js
+++ src/main/webapp/Controller/homeController.js
@@ -236,18 +236,6 @@ toDoApp.controller('homeController', function($scope, homeService, $uibModal, $l
 						}
 						
 
-					$scope.imageCrop = function() {
-						$scope.cropper = {};
-						$scope.cropper.sourceImage = null;
-						$scope.cropper.croppedImage = null;
-						$scope.bounds = {};
-						$scope.bounds.left = 0;
-						$scope.bounds.right = 0;
-						$scope.bounds.top = 0;
-						$scope.bounds.bottom = 0;
-					}
-						
-						
 						
 			/*---------------------------------Social Share-----------------------------------------*/
 						
